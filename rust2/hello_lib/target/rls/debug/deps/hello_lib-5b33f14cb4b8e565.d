4e3b0b78a228fbdfc2a0ff0d34afbcb37ebd4d94
Carlos <carlos.araya@gmail.com>
2020-12-28 22:27:57 -0800

    initial push, part 2

--- rust2/hello_lib/target/rls/debug/deps/hello_lib-5b33f14cb4b8e565.d
+++
@@ -1,5 +0,0 @@
-/Users/carlos/code/bazel-demo/app/rust2/hello_lib/target/rls/debug/deps/hello_lib-5b33f14cb4b8e565.rmeta: src/lib.rs
-
-/Users/carlos/code/bazel-demo/app/rust2/hello_lib/target/rls/debug/deps/hello_lib-5b33f14cb4b8e565.d: src/lib.rs
-
-src/lib.rs:
